f2b8df91d8b009f5f4965a56099d69b854fdb8a5
HuberAdrian <huberadrian@web.de>
2023-02-17 11:02:22 +0100

    added comments

--- server/index.js
+++ server/index.js
@@ -3,8 +3,8 @@ const app = express();
 const cors = require("cors");
 const port = 3042;
 
-app.use(cors());
-app.use(express.json());
+app.use(cors()); // is used to enable Cross-Origin Resource Sharing for the Express application. This means that the server will be able to receive requests from different domains and allow access to the resources accordingly. Without CORS, browsers may block requests to the server, making it difficult for clients to interact with the server's resources.
+app.use(express.json()); // is used to parse JSON bodies from requests. This is used to parse the request body sent by the client.
 
 const balances = {
   "0x1": 100,
@@ -12,12 +12,14 @@ const balances = {
   "0x3": 75,
 };
 
+// GET /balance/:address
 app.get("/balance/:address", (req, res) => {
   const { address } = req.params;
   const balance = balances[address] || 0;
   res.send({ balance });
 });
 
+// POST /send
 app.post("/send", (req, res) => {
   const { sender, recipient, amount } = req.body;
 
@@ -32,6 +34,7 @@ app.post("/send", (req, res) => {
     res.send({ balance: balances[sender] });
   }
 });
+
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}!`);
