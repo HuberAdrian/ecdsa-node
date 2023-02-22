eeb75b064ee89059596f1964d7cf734ea63748e9
HuberAdrian <huberadrian@web.de>
2023-02-22 11:12:08 +0100

    added functions for hashing and sign transactions

--- client/src/Transfer.jsx
+++ client/src/Transfer.jsx
@@ -1,14 +1,38 @@
 import { useState } from "react";
 import server from "./server";
+import { keccak256 } from "ethereum-cryptography/keccak";
+import { utf8ToBytes } from "ethereum-cryptography/utils";
+import secp from "ethereum-cryptography/secp256k1";
+
+const PRIVATE_KEY = "6b911fd37cdf5c81d4c0adb1ab7fa822ed253ab0ad9aa18d77257c88b29b718e";
+
+
+function hashMessage(message) {
+    const bytes = utf8ToBytes(message)
+    const hash = keccak256(bytes)
+
+    return hash
+}
+
+async function signMessage(msg) {
+  const hash = hashMessage(msg)
+  const signature = await secp.sign(hash, PRIVATE_KEY, {recovered: true});
+  
+  return signature
+}
+
 
 function Transfer({ address, setBalance }) {
   const [sendAmount, setSendAmount] = useState("");
   const [recipient, setRecipient] = useState("");
+  const [privateKey, setPrivateKey] = useState("");
 
   const setValue = (setter) => (evt) => setter(evt.target.value);
 
   async function transfer(evt) {
     evt.preventDefault();
+    const signature = await signMessage(sendAmount);
+    
 
     try {
       const {
@@ -32,6 +56,15 @@ function Transfer({ address, setBalance }) {
         Send Amount
         <input
           placeholder="1, 2, 3..."
+          value={privateKey}
+          onChange={setValue(setPrivateKey)}
+        ></input>
+      </label>
+
+      <label>
+        Private Key
+        <input
+          placeholder="Type in Private Key, for example: 0x1"
           value={sendAmount}
           onChange={setValue(setSendAmount)}
         ></input>
--- server/index.js
+++ server/index.js
@@ -2,6 +2,25 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const port = 3042;
+const secp = require("ethereum-cryptography/secp256k1");
+const { keccak256 } = require("ethereum-cryptography/keccak");
+const { utf8ToBytes } = require("ethereum-cryptography/utils");
+
+function hashMessage(message) {
+    const bytes = utf8ToBytes(message)
+    const hash = keccak256(bytes)
+
+    return hash
+}
+
+
+
+async function recoverKey(message, signature, recoveryBit) {
+    const hash = hashMessage(message)
+    const pubKey = await secp.recoverPublicKey(hash, signature, recoveryBit)
+
+    return pubKey
+}
 
 app.use(cors()); // is used to enable Cross-Origin Resource Sharing for the Express application. This means that the server will be able to receive requests from different domains and allow access to the resources accordingly. Without CORS, browsers may block requests to the server, making it difficult for clients to interact with the server's resources.
 app.use(express.json()); // is used to parse JSON bodies from requests. This is used to parse the request body sent by the client.
