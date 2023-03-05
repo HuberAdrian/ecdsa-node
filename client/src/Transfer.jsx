f2e2b5a88f6c380b3b13bd95da5572437013501e
HuberAdrian <huberadrian@web.de>
2023-03-05 21:35:14 +0100

    resolved merge conflicts

--- client/src/Transfer.jsx
+++ client/src/Transfer.jsx
@@ -2,25 +2,10 @@ import * as secp from "ethereum-cryptography/secp256k1";
 import { sha256 } from "ethereum-cryptography/sha256";
 import { toHex, utf8ToBytes } from "ethereum-cryptography/utils";
 import server from "./server";
-import * as secp from "ethereum-cryptography/secp256k1"
-import { keccak256 } from "ethereum-cryptography/keccak";
-import { toHex, utf8ToBytes } from "ethereum-cryptography/utils";
 
 function Transfer({ privateKey, setBalance }) {
   async function handleSubmit(event) {
     event.preventDefault();
-
-    const message = {
-      address, sendAmount, recipient,
-    };
-
-    const hash = hashMessage(JSON.stringify(message));
-    const [sig, recoveryBit] = await secp.sign(hash, privateKey, {recovered: true});
-    message.sign = toHex(sig);
-    message.recoveryBit = recoveryBit;
-    setSignature(toHex(sig));
-    setRecoveryBit(recoveryBit);
-    console.log('request:', message);
 
     try {
       const data = {
